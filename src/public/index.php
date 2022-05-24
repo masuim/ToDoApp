a7f4a0ca8681e69e2936c0e6f58bb7cbf243813e
masuim <po.n31227@gmail.com>
2022-05-24 12:13:10 +0900

    refact : 見た目とテキストの修正

--- src/public/index.php
+++ src/public/index.php
@@ -75,82 +75,85 @@ if(isset($_POST['isComplete'])){
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <title>タスク一覧</title>
   </head>
-  <body>
+  <body class="text-gray-700">
     <header>
       <?php require_once __DIR__ . '/../app/Lib/header.php'; ?>
     </header>
-    <div class="w-4/5 bg-white ml-20 mr-20 mt-10 justify-center items-center shadow-xl">
+    <div class="text-sm w-4/5 bg-white ml-20 mr-20 mt-10 pl-10 py-10 justify-center items-center rounded-lg shadow-xl">
       <form action="" method="post">
         <div class="inline">
-          <input class="border-black h-5 w-50 p-4 border-2 bg-white" type="textarea" name="searchWord" placeholder="タスク名キーワード入力" value="<?php echo $search ??
+          <input class="border-gray-700 h-5 w-50 p-4 mx-5 border-2 rounded" type="textarea" name="searchWord" placeholder="タスク名キーワード入力" value="<?php echo $search ??
               ''; ?>">
         </div>
-        <div class="inline">
+        <div class="inline mr-5">
           <label><input type="radio" name="order" value="asc">新しい順</label>
           <label><input type="radio" name="order" value="desc">古い順</label>
         </div>
-        <div class="inline">
+        <div class="inline mr-5">
           <select name="selectCategory">
             <option value=""><?php echo "カテゴリ"; ?></option>
-              <?php foreach ( $getCategories as $value ) : ?>
-                <option class="text-right" value="<?php echo $value["name"]; ?>" name="categoryName"><?php echo $value["name"]; ?></option>
-              <? endforeach; ?>
+            <?php foreach ( $getCategories as $value ) : ?>
+              <option class="text-right" value="<?php echo $value["name"]; ?>" name="categoryName"><?php echo $value["name"]; ?></option>
+            <? endforeach; ?>
           </select>
         </div>      
-        <div class="mb-5 inline">
+        <div class="mb-5 mr-5 inline">
           <label><input type="radio" name="isComplete" value="inComplete">未完了</label>
           <label><input type="radio" name="isComplete" value="complete">完了</label>
         </div>
-        <button type="submit" class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">送信</button>
+        <button type="submit" class="inline bg-indigo-400 hover:bg-indigo-700 text-white mt-1 mr-5 py-1 px-4 rounded">検索・並び替え・絞り込み</button>
+        <button type="submit" class="inline border border-indigo-700 text-indigo-400 hover:bg-indigo-200 text-white mt-1 py-1 px-4 rounded">元に戻す</button>
       </form>
     </div>
-    <div class="bg-white ml-20 mr-20 mt-10 justify-center items-center">
+    <div class="ml-20 mr-20 mt-10 justify-center items-center">
       <div>
-        <button class="text-blue-500 py-3 px-4"><a href="./task/create.php">タスクを追加</a></button>
+        <button class="text-green-500 py-3 px-4"><a href="./task/create.php">タスクを追加</a></button>
       </div>
-      <table class="table-auto">
-        <thead>
-          <tr>
-            <th class="px-4 py-2">タスク名</th>
-            <th class="px-4 py-2">締め切り</th>
-            <th class="px-4 py-2">カテゴリー名</th>
-            <th class="px-4 py-2">完了未完了</th>
-            <th class="px-4 py-2">編集</th>
-            <th class="px-4 py-2">削除</th>
-          </tr>
-        </thead>
-        <?php if ($taskForWeb): ?>
-          <?php foreach ($taskForWeb as $task): ?>
-            <tbody>
-              <tr>
-                <td class="border px-4 py-2"><?php echo $task['contents']; ?></td>
-                <td class="border px-4 py-2"><?php echo $task['deadline']; ?></td>
-                <td class="border px-4 py-2"><?php echo $task['name']; ?></td>
-                <?php if ($task["status"] == 0) : ?>
+      <div class="flex justify-center">
+        <table class="table-auto">
+          <thead>
+            <tr class="bg-indigo-50">
+              <th class="px-4 py-2 border">タスク名</th>
+              <th class="px-4 py-2 border">締め切り</th>
+              <th class="px-4 py-2 border">カテゴリー名</th>
+              <th class="px-4 py-2 border ">完了未完了</th>
+              <th class="px-4 py-2 border">編集</th>
+              <th class="px-4 py-2 border">削除</th>
+            </tr>
+          </thead>
+          <?php if ($taskForWeb): ?>
+            <?php foreach ($taskForWeb as $task): ?>
+              <tbody>
+                <tr>
+                  <td class="border px-4 py-2"><?php echo $task['contents']; ?></td>
+                  <td class="border px-4 py-2"><?php echo $task['deadline']; ?></td>
+                  <td class="border px-4 py-2"><?php echo $task['name']; ?></td>
+                  <?php if ($task["status"] == 0) : ?>
+                    <td class="border px-4 py-2 text-indigo-600">
+                      <a href='__DIR__."/../task/updateStatus.php?id=<?php echo $task['id']; ?>&status=<?php echo $task["status"]; ?>" '>未完了</a>
+                    </td>
+                  <?php endif; ?>
+                  <?php if ($task["status"] == 1) : ?>
+                    <td class="border px-4 py-2 text-indigo-600 text-opacity-50">
+                      <a href='__DIR__."/../task/updateStatus.php?&id=<?php echo $task['id']; ?>&status=<?php echo $task["status"]; ?>" '>完了</a>
+                    </td>
+                  <?php endif; ?>              
                   <td class="border px-4 py-2">
-                    <a href='__DIR__."/../task/updateStatus.php?id=<?php echo $task['id']; ?>&status=<?php echo $task["status"]; ?>" '>未完了</a>
+                    <button class="bg-indigo-400 hover:bg-indigo-700 text-white py-3 px-4 m-1 rounded">
+                      <a href="./task/edit.php?taskId=<?php echo $task['id']; ?>">編集</a>
+                    </button>
                   </td>
-                <?php endif; ?>
-                <?php if ($task["status"] == 1) : ?>
                   <td class="border px-4 py-2">
-                    <a href='__DIR__."/../task/updateStatus.php?&id=<?php echo $task['id']; ?>&status=<?php echo $task["status"]; ?>" '>完了</a>
+                    <button class="bg-red-400 hover:bg-red-600 text-white py-3 px-4 rounded">
+                      <a href="./task/delete.php?taskId=<?php echo $task['id']; ?>">削除</a>
+                    </button>
                   </td>
-                <?php endif; ?>              
-                <td class="border px-4 py-2">
-                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 m-1 rounded">
-                    <a href="./task/edit.php?taskId=<?php echo $task['id']; ?>">編集</a>
-                  </button>
-                </td>
-                <td class="border px-4 py-2">
-                  <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-4 rounded">
-                    <a href="./task/delete.php?taskId=<?php echo $task['id']; ?>">削除</a>
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          <?php endforeach; ?>
-        <?php endif; ?>
-      </table>
+                </tr>
+              </tbody>
+            <?php endforeach; ?>
+          <?php endif; ?>
+        </table>
+      </div>
     </div> 
   </body>
 </html>
--- src/public/user/signup.php
+++ src/public/user/signup.php
@@ -20,23 +20,23 @@ $mail = $formInputs['mail'] ?? '';
     <title>アカウントの作成</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   </head>
-  <body class="bg-gray-200 w-full h-screen flex justify-center items-center">
-    <div class="w-96  bg-white pt-10 pb-10 rounded-xl">
+  <body class="bg-indigo-100 w-full h-screen flex justify-center items-center">
+    <div class="w-96 bg-white pt-10 pb-10 rounded-xl">
       <div class="w-60 m-auto text-center">
-        <h2 class="text-2xl pb-5">会員登録</h2>
-          <?php if ($errors): ?>
-            <?php foreach ($errors as $error): ?>
-              <p class="text-red-600"><?php echo $error; ?></p>
-            <?php endforeach; ?>
-          <?php endif; ?>
+        <h2 class="text-2xl text-gray-700 pb-5">会員登録</h2>
+        <?php if ($errors): ?>
+          <?php foreach ($errors as $error): ?>
+            <p class="text-red-600"><?php echo $error; ?></p>
+          <?php endforeach; ?>
+        <?php endif; ?>
         <form action="./signup_complete.php" method="POST">
           <p><input class='border-2 border-gray-300 w-full mb-5' placeholder="User name" type=“text” name="userName" required value="<?php echo $userName; ?>"></p>
           <p><input class='border-2 border-gray-300 w-full mb-5' placeholder="Email" type=“mail” name="mail" required value="<?php echo $mail; ?>"></p>
           <p><input class='border-2 border-gray-300 w-full mb-5' placeholder="Password" type="password" name="password"></p>
           <p><input class='border-2 border-gray-300 w-full mb-5' placeholder="Password確認" type="password" name="confirmPassword"></p>
-          <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mb-5 w-full' type="submit">アカウント作成</button>
+          <button class='bg-indigo-400 hover:bg-indigo-700 text-white py-1 px-2 rounded mb-5 w-full' type="submit">アカウント作成</button>
         </form>
-        <a class="text-blue-600" href="./signin.php">ログイン画面へ</a>
+        <a class="text-indigo-600" href="./signin.php">ログイン画面へ</a>
       </div>
     </div>
   </body>
